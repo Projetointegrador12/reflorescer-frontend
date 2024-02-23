2f2741071ec8d692e45bc7ccd7e3c48081420e92
Daniela Xavier <danielamxsoares@gmail.com>
2024-02-23 09:59:51 -0300

    feat: criar estrutura de context e service

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,67 @@
+import { createContext, ReactNode, useState } from "react";
+import UsuarioLogin from "../model/UsuarioLogin";
+import { login } from "../service/Service";
+
+// import { toastAlerta } from "../utils/toastAlerta"
+
+interface AuthContextProps {
+  usuario: UsuarioLogin;
+  handleLogout(): void;
+  handleLogin(usuario: UsuarioLogin): Promise<void>;
+  isLoading: boolean;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextProps);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [usuario, setUsuario] = useState<UsuarioLogin>({
+    id: 0,
+    nomeUsuario: "",
+    usuario: "",
+    senha: "",
+    foto: "",
+    token: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleLogin(userLogin: UsuarioLogin) {
+    setIsLoading(true);
+    try {
+      await login(/usuarios/aglor, userLogin, setUsuario);
+      alert("Usuário logado com sucesso");
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      alert("Dados do usuário inconsistentes");
+      setIsLoading(false);
+    }
+  }
+
+  function handleLogout() {
+    setUsuario({
+      id: 0,
+      nomeUsuario: "",
+      usuario: "",
+      senha: "",
+      foto: "",
+      token: "",
+    });
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ usuario, handleLogin, handleLogout, isLoading }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function login(arg0: RegExp, userLogin: UsuarioLogin, setUsuario: any) {
+  throw new Error("Function not implemented.");
+}
